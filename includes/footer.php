807b95e50f5e0c0e78b1cdb1bb071a80e5a014b3
Habu Matano <117619833+Ma5Codes@users.noreply.github.com>
2024-03-03 10:45:39 +0300

    Add files via upload

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer class="py-5 bg-inverse">
+    <div class="container">
+        <p class="m-0 text-center text-white">Copyright &copy; BloodBank & Donor Management System 2024. Brought To You By <a href="#">Ma5Codes</a></p>
+    </div>
+</footer>
